c18727651aed0d66b7e3d1594d89787f3e045765
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-09 02:13:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.any? { |word| return (word.length > 4 ? true : false) }
+  words.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese(snacks)
