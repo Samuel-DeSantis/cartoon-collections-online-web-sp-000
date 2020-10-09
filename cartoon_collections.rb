7d9a51d1f679f32ea6814aee3d7c3cd5958e200d
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-10-09 02:02:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index { |name, index| puts "#{index + 1} #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.find { |word| return (word.length > 4 ? true : false) }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if snacks.include?(cheese_types)
+    snacks.find { |snack| return snack if (snack === cheese_types[0] || snack === cheese_types[1] || snack === cheese_types[2]) }
+  else
+    return nil
+  end
 end
